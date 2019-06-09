239f253bf6c5f2d12e748ba6c8ef4b5c6ab1d381
nhovay8 <nhovay8@gmail.com>
2019-06-09 15:23:30 +0700

    Update Login form

--- src/quan/com/HelloServlet.java
+++ src/quan/com/HelloServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HelloServlet
  */
 //http://localhost:8080/ProjectServlet/HelloServlet
-@WebServlet(description = "This is Servlet Hello World", urlPatterns = { "/HelloServlet" })
+@WebServlet(description = "This is Servlet Hello World", urlPatterns = { "/login" })
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -32,15 +32,26 @@ public class HelloServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		PrintWriter printWriter = response.getWriter();
-		printWriter.print("Hello World ............");
+		printWriter.print("Hello World By Get............");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter printWriter = response.getWriter();
+		printWriter.println("Hello World By Post............<br/>");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		
+		if(username.equals("abcd") && password.equals("123456")) {
+			printWriter.println("hello:" + username);
+		} else {
+			printWriter.println("username or password is wrong ...");
+		}
+		
 	}
 
 }
